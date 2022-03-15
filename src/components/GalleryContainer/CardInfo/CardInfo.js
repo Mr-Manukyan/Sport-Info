9948025b0bf8f535f1e92ab71f087c3632b6c44f
Mr-Manukyan <mrkinoman1915@gmail.com>
2022-03-15 19:44:23 +0400

    fix

--- src/components/GalleryContainer/CardInfo/CardInfo.js
+++ src/components/GalleryContainer/CardInfo/CardInfo.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import {useParams,useNavigate } from 'react-router-dom'
+import {useNavigate } from 'react-router-dom'
 import style from  './CardInfo.module.css'
 import { data } from '../Data'
 

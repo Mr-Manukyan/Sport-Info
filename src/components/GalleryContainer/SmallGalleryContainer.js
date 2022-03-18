35f73cd0137d052cd5c51be8b53ad28e3a41955f
Mr-Manukyan <mrkinoman1915@gmail.com>
2022-03-18 16:31:13 +0400

    fix

--- src/components/GalleryContainer/SmallGalleryContainer.js
+++ src/components/GalleryContainer/SmallGalleryContainer.js
@@ -1,26 +1,14 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import style from "./SmallGalleryContainer.module.css";
 import { motion } from "framer-motion";
 import { data } from "./Data";
 import Card from "./Card/Card";
-import { BsArrowLeftCircleFill } from "react-icons/bs";
-import { BsArrowRightCircleFill } from "react-icons/bs";
 import { LanguageContext } from "../Common/LanguageProvider/LanguageProvider";
-import smallBG from  '../Asset/photos/galleryPhotos/smallBG.jpg'
 
 
-const animationsButton = {
-  initial: {
-    opacity: 0,
-    scale: 0.3,
-    y: "-500px",
-  },
-  animate: {
-    opacity: 1,
-    scale: 1,
-    y: 0,
-  },
-};
+
+
+
 const animationsParagraph = {
   initial: {
     opacity: 0,
@@ -47,55 +35,29 @@ const animationsCard = {
 };
 
 const SmallGalleryContainer = () => {
-  const [offset, setOffset] = useState(0);
+
   const { dictionary } = useContext(LanguageContext);
   const galleryParagraph = dictionary.galleryParagraph;
-  const PAGE_WIDTH = window.innerWidth >= 540 ? 510 : 300;
 
-  const handleLeftArrowClick = () => {
-
-    setOffset((currentOffset) => {
-      const newOffset = currentOffset + PAGE_WIDTH;
-      return Math.min(newOffset, 0);
-    });
-  };
-  const handleRightArrowClick = () => {
-
-    setOffset((currentOffset) => {
-      const newOffset = currentOffset - PAGE_WIDTH;
-      const maxOffset = -(PAGE_WIDTH * (data.length - 1));
-      return Math.max(newOffset, maxOffset);
-    });
-  };
 
   return (
-    <div className={style.container} style = {{
-                                               background:`url(${window.innerWidth <= 720 && smallBG})`,
-                                               backgroundRepeat:'no-repeat',
-                                               backgroundSize:'cover',
-                                               backgroundPosition : 'center'
-                                              
-                                              }}>
-      <motion.div className={style.wrapper} 
+    <div className={style.container}>
+
+      <motion.p className={style.headerParagraph}
+                initial="initial"
+                animate="animate"
+                variants={animationsParagraph}
+                transition={{ duration: 0.6, delay: 1 }}
+          >
+        {galleryParagraph}
+      </motion.p>
+
+      <motion.div className={style.wrapper}    
            initial="initial"
            animate="animate"
            variants={animationsCard}
            transition={{ duration: 0.4}}
       >
-        <motion.p
-          className={style.headerParagraph}
-          initial="initial"
-          animate="animate"
-          variants={animationsParagraph}
-          transition={{ duration: 0.6, delay: 1 }}
-        >
-          {galleryParagraph}
-        </motion.p>
- 
-        <div className={style.window}>
-          <div  className={style.allItemsContainer}
-                style={{ transform: `translateX(${offset}px)` }}
-          >
             {data.map((card, index) => (
               <Card
                 key={card.id}
@@ -105,35 +67,10 @@ const SmallGalleryContainer = () => {
                 small={true}
               />
             ))}
-          </div>
-        </div>
+       
+     
     
       </motion.div>
-  
-      <div className={style.buttonWrapper}>
-        <motion.button
-          className={style.button}
-          initial="initial"
-          animate="animate"
-          variants={animationsButton}
-          transition={{ duration: 0.5, delay: 1.5 }}
-          onClick={handleLeftArrowClick}
-        >
-          {<BsArrowLeftCircleFill />}
-        </motion.button>
-
-        <motion.button
-          className={style.button}
-          initial="initial"
-          animate="animate"
-          variants={animationsButton}
-          transition={{ duration: 0.5, delay: 1.5 }}
-          onClick={handleRightArrowClick}
-        >
-          {<BsArrowRightCircleFill />}
-        </motion.button>
-      </div>
-    
     </div>
   );
 };
